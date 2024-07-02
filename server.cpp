10f8505bd657322ee4883ba8ce10cbc5960bcf01
Soaib024 <Soaib024@gmail.com>
2024-07-02 20:22:23 +0530

    rm ununsed methods

--- server.cpp
+++ server.cpp
@@ -47,108 +47,11 @@ static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
 }
 
-static void do_something(int connfd){
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-
-    if(n < 0){
-        msg("read() error");
-        return;
-    }
-
-    printf("client says: %s\n", rbuf);
-    char wbuf[] = "world\n";
-    write(connfd, wbuf, strlen(wbuf));
-}
-
-
-static int32_t read_full(int fd, char *buf, size_t n){
-    while(n > 0){
-        /*
-        The read() syscall just returns whatever data is available in the kernel, 
-        or blocks if there is none. It’s up to the application to handle insufficient
-        data. The read_full() function reads from the kernel until it gets exactly n bytes.
-        */
-        ssize_t rv = read(fd, buf, n);
-        if(rv <= 0){
-            return -1; // error or unexpected EOF
-        }
-
-        assert((size_t)rv <= n);
-        n -= (size_t)rv;
-        buf += rv;
-    }
-
-    return 0;
-}
-
-static int32_t write_full(int fd, const char *buf, size_t n){
-    while(n > 0){
-        /*
-        The write() syscall may return successfully with partial data written if the kernel buffer is full,
-        we must keep trying when the write() returns fewer bytes than we need.
-        */
-        ssize_t rv = write(fd, buf, n);
-        if(rv <= 0){
-            return -1; // error
-        }
-
-        assert((size_t)rv <= n);
-        n -= (size_t)rv;
-        buf += rv;
-    }
-    return 0;
-}
-
 /*
 +-----+------+-----+------+--------
 | len(4B) | msg1 | len(4B) | msg2 | more...
 +-----+------+-----+------+--------
 */
-
-static int32_t one_request(int connfd){
-    // 4B for len
-    char rbuf[4 + k_max_msg + 1];
-
-    errno = 0;
-    int32_t err = read_full(connfd, rbuf, 4);
-
-    if(err){
-        if(errno == 0){
-            msg("EOF");
-        }else{
-            msg("read() error");
-        }
-        return err;
-    }
-
-    uint32_t len = 0;
-    // read first 4B from the buffer to len variable, assume little endian
-    memcpy(&len, rbuf, 4);
-
-    if(len > k_max_msg){
-        msg("to long");
-        return -1;
-    }
-
-    err = read_full(connfd, &rbuf[4], len);
-    if(err){
-        msg("read() error");
-        return err;
-    }
-
-    // do something
-    rbuf[4 + len] = '\0';
-    printf("client says: %s\n", &rbuf[4]);
-
-    // reply using the same protocol
-    const char reply[] = "world";
-    char wbuf[4 + sizeof(reply)];
-    len = (uint32_t)strlen(reply);
-    memcpy(wbuf, &len, 4);
-    memcpy(&wbuf[4], reply, len);
-    return write_full(connfd, wbuf, 4 + len);
-}
 
 static void fd_set_nb(int fd) {
     errno = 0;  // Clear errno before calling fcntl
@@ -448,7 +351,7 @@ int main(){
             struct pollfd pfd = {};
             pfd.fd = conn->fd;
             pfd.events = (conn->state == STATE_REQ) ? POLL_IN: POLL_OUT;
-            pfd.events == pfd.events | POLL_ERR;
+            pfd.events = (pfd.events | POLL_ERR);
             poll_args.push_back(pfd);
         }
 
